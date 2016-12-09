9e15233daba63e155516f68b4a35146e7b6b8b25
robertelving <vivalaklask@gmail.com>
2016-12-09 13:19:27 +0100

    Initial Commit

---
+++ DesignByContract2/Customer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace DesignByContract2
+{
+	public class Customer
+	{
+
+		private int id { get; }
+		private string name { get; }
+
+		[ContractInvariantMethod]
+		private void ObjectInvariant()
+		{
+			Contract.Invariant(id > 0);
+			Contract.Invariant(!String.IsNullOrWhiteSpace(name));
+		}
+
+		public Customer(int id, string name)
+		{
+			this.id = id;
+			this.name = name;
+			Contract.Ensures(id > 0);
+		}
+
+		static void Main(string[] args)
+		{
+
+			var c = new Customer(-1 , "Robert");
+
+			Console.WriteLine("Customer: {0} - {1}", c.id, c.name);
+			Console.ReadKey();
+		}
+
+	}
+
+}
---
+++ DesignByContract2/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace DesignByContract2
+{
+	class MainClass
+	{
+		
+	}
+}
