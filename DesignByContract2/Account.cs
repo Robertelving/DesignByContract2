0ed3e9aeb8f03d15a6aa1d6a89aec47a8ffe96c1
robertelving <vivalaklask@gmail.com>
2016-12-10 14:37:03 +0100

    added skeleton to Account

--- DesignByContract2/Account.cs
+++ DesignByContract2/Account.cs
@@ -1,10 +1,26 @@
 ﻿using System;
+using System.Diagnostics.Contracts;
+
 namespace DesignByContract2
 {
 	public class Account
 	{
-		public Account()
+
+		private long number { get; }
+		private double balance { get;}
+
+		public Account(int number, double balance)
 		{
+			this.number = number;
+			this.balance = balance;
 		}
+
+		[ContractInvariantMethod]
+		private void ObjectInvariant()
+		{
+			Contract.Invariant(id > 0);
+			Contract.Invariant(!String.IsNullOrWhiteSpace(name));
+		}
+
 	}
 }
