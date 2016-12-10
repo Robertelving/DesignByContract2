cc2ff949ac4fc9ac6eb8ccf5dd7b782f0a3fe157
robertelving <vivalaklask@gmail.com>
2016-12-10 14:52:20 +0100

    Added methods to Bank Class

--- DesignByContract2/Bank.cs
+++ DesignByContract2/Bank.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 namespace DesignByContract2
 {
-	public class EmptyClass
+	public class Bank
 	{
-		public EmptyClass()
+
+		private string name { get; }
+
+		public Bank()
 		{
+		}
+
+		public bool move(double amount, Account source, Account target) {
+			throw new NotSupportedException();
+		}
+
+		public string makeStatement(Customer customer, Account account) { 
+			throw new NotSupportedException();
 		}
 	}
 }
